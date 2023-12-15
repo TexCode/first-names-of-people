cac26cc5ccef8087fcf27cd95e01c511fd5d8b9b
kamal <kamal@example.com>
2023-12-15 19:24:47 +0000

    First Names of the People

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const arrayOfPersonNames = require("../country/state/city/index.js");
+const getFirstNames = require("../utilities/utils/index.js");
+
+const getPeopleInCity = (list) => {
+  return getFirstNames(list);
+};
+
+module.exports = getPeopleInCity;
